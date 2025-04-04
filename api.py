3dc759c0fd524f6f681f455b6b999b285174ad64
Nividu007 <hirosh@nividu.com>
2025-04-04 22:50:13 +0530

    1.0

---
+++ api.py
@@ -0,0 +1,45 @@
+from flask import Flask, request, jsonify, render_template
+from sklearn.preprocessing import StandardScaler
+import pandas as pd
+from flask_cors import CORS
+import joblib
+import numpy as np
+
+app = Flask(__name__)
+CORS(app)
+
+model = joblib.load('CropyModel.pkl')
+
+@app.route('/predict', methods=['GET'])
+def predict():
+    try:
+        input_data = {
+            'Crop': request.args.get('Crop'),
+            'Season': request.args.get('Season'),
+            'Area': float(request.args.get('Area')),
+            'Annual_Rainfall': float(request.args.get('Annual_Rainfall')),
+            'Fertilizer': float(request.args.get('Fertilizer')),
+            'Pesticide': float(request.args.get('Pesticide'))
+        }
+
+        if input_data['Area'] <= 0:
+            raise ValueError("Area must be greater than 0.")
+        if input_data['Fertilizer'] < 0 or input_data['Pesticide'] < 0 or input_data['Annual_Rainfall'] < 0:
+            raise ValueError("Fertilizer, Pesticide, and Annual Rainfall must be non-negative.")
+
+        input_df = pd.DataFrame([input_data])
+
+
+        input_df = pd.get_dummies(input_df, columns=['Crop', 'Season'])
+
+        model_features = model.feature_names_in_
+        input_df = input_df.reindex(columns=model_features, fill_value=0)
+
+        prediction = model.predict(input_df)[0]
+        return jsonify({'prediction': prediction})
+
+    except Exception as e:
+        return jsonify({'error': str(e)})
+
+if __name__ == '__main__':
+    app.run(debug=True)
